0f2ee792a275b1fe090178c3576d752ba0cc30ce
Gokul <89386101+th3gokul@users.noreply.github.com>
2023-01-18 07:41:18 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,172 @@
+clear
+echo "it will update Your os"
+
+sudo apt-get -y update
+sleep 5
+clear
+echo "Do you want to upgarde your os (sudo apt-get -y upgrade)? (yes/no)"
+read answer
+if [ "$answer" == "yes" ]; then
+    sudo apt-get -y upgrade
+    echo "upgrade success"
+else
+    if [ "$answer" == "no" ]; then
+        echo "Continuing with installing"
+    else
+        echo "Invalid input. Please type 'yes' or 'no'."
+    fi
+fi
+
+sudo apt-get install -y python3-pip
+sudo apt-get install -y git
+
+clear
+
+echo "Installing Golang"
+sleep 5
+
+# Check if golang is already installed
+if go version 2>/dev/null; then
+    echo "Golang is already installed. Proceeding to next step of program."
+else
+    echo "Golang is not installed. Installing Golang."
+    rm -rf /usr/local/go
+    wget https://dl.google.com/go/go1.19.5.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go
+    sudo tar -xvf go1.19.5.linux-amd64.tar.gz
+    sudo mv go /usr/local
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+
+    echo "Golang installed successful"
+
+fi
+
+clear
+
+echo "Now installing tools"
+sleep 4
+
+# Check if assetfinder is installed
+if ! [ -x "$(command -v assetfinder)" ]; then
+  echo 'Installing assetfinder...'
+  go install github.com/tomnomnom/assetfinder@latest
+  echo 'assetfinder has been installed.'
+else
+  echo 'assetfinder is already installed.'
+fi
+
+# Check if gf is installed
+if ! [ -x "$(command -v gf)" ]; then
+  echo 'Installing gf...'
+  go install github.com/tomnomnom/gf@latest
+  echo 'gf has been installed.'
+else
+  echo 'gf is already installed.'
+fi
+
+# Check if httprobe is installed
+if ! [ -x "$(command -v httprobe)" ]; then
+  echo 'Installing httprobe...'
+  go install github.com/tomnomnom/httprobe@latest
+  echo 'httprobe has been installed.'
+else
+  echo 'httprobe is already installed.'
+fi
+
+# Check if httpx is installed
+if ! [ -x "$(command -v httpx)" ]; then
+  echo 'Installing httpx...'
+  go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+  echo 'httpx has been installed.'
+else
+  echo 'httpx is already installed.'
+fi
+
+# Check if subfinder is installed
+if ! [ -x "$(command -v subfinder)" ]; then
+  echo 'Installing subfinder...'
+  go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+  echo 'subfinder has been installed.'
+else
+  echo 'subfinder is already installed.'
+fi
+
+# Check if waybackurls is installed
+if ! [ -x "$(command -v waybackurls)" ]; then
+  echo 'Installing waybackurls...'
+  go install github.com/tomnomnom/waybackurls@latest
+  echo 'waybackurls has been installed.'
+else
+  echo 'waybackurls is already installed.'
+fi
+
+# Check if subz is installed
+if ! [ -x "$(command -v subzy)" ]; then
+  echo 'Installing subz...'
+  go install -v github.com/lukasikic/subzy@latest
+  echo 'subz has been installed.'
+else
+  echo 'subz is already installed.'
+fi
+
+# Check if gau is installed
+
+if ! [ -x "$(command -v gau)" ]; then
+  echo 'Installing gau...'
+  go install github.com/lc/gau/v2/cmd/gau@latest
+  echo 'gau has been installed.'
+else
+  echo 'gau is already installed.'
+fi
+
+# Check if eyewitness is installed
+if ! [ -x "$(command -v eyewitness)" ]; then
+  echo 'Installing eyewitness...'
+  apt-get install -y eyewitness
+  echo 'eyewitness has been installed.'
+else
+  echo 'eyewitness is already installed.'
+fi
+
+mkdir ~/recon
+
+cd ~/recon && git clone https://github.com/devanshbatham/ParamSpider.git && cd ParamSpider
+pip3 install -r requirements.txt
+
+sleep 3
+
+clear
+echo "configure the all tools plzz wait"
+
+sleep 3
+
+sudo cd ~/go/bin
+sudo cp assetfinder gf httprobe httpx subfinder waybackurls subzy /usr/bin
+sudo cp gau /usr/bin/gau
+
+mkdir ~/.gf
+cd ~  && git clone https://github.com/1ndianl33t/Gf-Patterns.git
+cp Gf-Patterns/*.json ~/.gf
+
+echo "checking all tools are installed"
+sleep 3
+TOOLS=(assetfinder gf httprobe httpx subfinder waybackurls subzy gau eyewitness)
+
+tools_installed=true
+
+for tool in "${TOOLS[@]}"
+do
+    if ! [ -x "$(command -v $tool)" ]; then
+        echo "$tool is not installed"
+        tools_installed=false
+    else
+        echo "$tool is already installed"
+    fi
+done
+
+if [ "$tools_installed" = true ]; then
+    echo "All tools are installed"
+    echo "installation completed"
+fi
+
---
+++ recon-hunter.sh
@@ -0,0 +1,262 @@
+TOOLS=(assetfinder gf httprobe httpx subfinder waybackurls subzy gau eyewitness)
+
+for tool in "${TOOLS[@]}"
+do
+    if ! command -v "$tool" > /dev/null 2>&1; then
+        echo "$tool is not installed"
+        exit 1
+    else
+        echo "$tool is already installed"
+    fi
+done
+
+echo "All tools are installed. Continuing with program..."
+sleep 4
+clear
+
+#!/bin/bash
+
+if [ ! -d ~/Desktop/output ]; then
+  mkdir ~/Desktop/output
+  echo "Output folder created in Desktop. Continuing with program..."
+else
+  echo "Output folder already exists in Desktop. Continuing with program..."
+fi
+
+clear
+
+# Get the target domain and company name
+
+echo -n "Enter the target domain: "
+read domain
+echo -n "Enter the company name: "
+read company
+
+# Create a new folder with the company name
+mkdir ~/Desktop/output/$company
+
+while true; do
+    echo -en "\n1 for subdomain & httpx & httprobe & screenshot &subzy\n2 for gau & waybackurl & gf\n3 for exit\nenter:" 
+    read input
+    if [ "$input" == "1" ]; then
+        echo "Executing program 1"
+        
+        sleep 3
+
+        mkdir ~/Desktop/output/$company/result1
+
+        # Move into the new folder
+        cd ~/Desktop/output/$company/result1
+
+
+        # Use subfinder to find subdomains
+        subfinder -d $domain > ~/Desktop/output/$company/result1/subfinder.txt
+
+        clear
+
+        echo "starting assetfinder"
+
+        # Use assetfinder to find subdomains
+        assetfinder --subs-only $domain > ~/Desktop/output/$company/result1/assetfinder.txt
+
+        echo "---assentfinder----finished----"
+        sleep 2
+
+        clear
+
+        echo "Remove Duplicates string and merge output"
+
+
+
+        cd ~/Desktop/output/$company/result1
+
+        # Remove duplicates
+        sort -u subfinder.txt -o subfinder.txt
+        sort -u assetfinder.txt -o assetfinder.txt
+
+        # Concatenate results and save to all-subdomains.txt
+        cat subfinder.txt assetfinder.txt | sort -u > all-subdomains.txt
+
+        sleep 2
+
+        clear
+
+        echo "-------httpx---starting---------"
+
+        sleep 3
+
+        # Use httpx to check status code
+        cat all-subdomains.txt | httpx -silent -status-code | tee status-code.txt
+
+        sleep 3
+
+        clear
+
+        echo "------httprobe----starting-----"
+
+
+        # Use httprobe to find live subdomains
+        cat all-subdomains.txt | httprobe -c 50 -t 2000 | tee -a all-live-subdomains.txt
+
+        sleep 3
+
+        clear
+
+
+        echo "extract https and http subdomains to live-http.txt"
+        sleep 3
+ 
+
+        # Use grep to extract https and http subdomains
+        grep "http" all-live-subdomains.txt > live-http.txt
+
+
+        # Use eyewitness to take screenshots
+
+        eyewitness -f all-subdomains.txt -d eyewitness
+
+        clear
+
+        #subzy subdomain takeover
+
+        subzy -targets all-subdomains.txt | tee subdomain-takeover.txt
+
+        
+
+    elif [ "$input" == "2" ]; then
+        echo "Executing program 2"
+
+        clear
+
+
+        mkdir ~/Desktop/output/$company/result2
+        cd ~/Desktop/output/$company/result2
+
+        #gau
+
+        echo "-----gau ------- starting----"
+
+        gau $domain | tee ~/Desktop/output/$company/result2/$company-gau.txt
+        echo "finish gau"
+
+        clear
+
+        #waybackurls
+
+        echo "-------waybackurls-----staring------"
+
+        waybackurls $domain | tee ~/Desktop/output/$company/result2/$company-waybackurls.txt
+        echo "finish waybackurls"
+        clear
+
+        cd ~/Desktop/output/$company/result2
+
+        sort -u $company-gau.txt -o $company-gau.txt
+        sort -u $company-waybackurls.txt -o $company-waybackurls.txt
+
+        cat $company-gau.txt $company-waybackurls.txt > all-url-gau-waybackurls.txt
+
+
+        echo "------paramspider--is---Loading-------"
+
+        sleep 3
+
+        echo "------Paramspider start-----------"
+
+        cd ~/my-stuff/Recon/ParamSpider/ && python3 paramspider.py --domain $domain | tee ~/Desktop/output/$company/result2/$company-paramspider.txt
+
+        echo "-----extracting paramspider result to http only -> http-paramspider.txt"
+        sleep 2
+
+        grep "http" ~/Desktop/output/$company/result2/$company-paramspider.txt > ~/Desktop/output/$company/result2/http-$company-paramspider.txt
+
+        mkdir ~/Desktop/output/$company/result2/gf-outputs
+        mkdir ~/Desktop/output/$company/result2/gf-outputs/gf-paramspider
+
+        echo "----gf----loading----using----paramspider----output-file----"
+
+        sleep 2
+
+        echo "-----start----gf----xss"
+
+    
+
+        gf xss ~/Desktop/output/$company/result2/$company-paramspider.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-paramspider/xss-gf.txt
+
+        sleep 3
+
+        clear
+
+        echo "-----start----gf----redirect"
+
+      
+
+        gf redirect ~/Desktop/output/$company/result2/$company-paramspider.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-paramspider/redirect-gf.txt
+        sleep 3
+
+        clear
+
+        echo "-----start----gf----sqli"
+
+    
+
+        gf sqli ~/Desktop/output/$company/result2/$company-paramspider.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-paramspider/sqli-gf.txt
+        sleep 3
+
+        clear
+
+
+        echo "-----start----gf----idor"
+
+        gf idor ~/Desktop/output/$company/result2/$company-paramspider.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-paramspider/sqli-gf.txt
+        sleep 3
+
+        clear
+
+        mkdir ~/Desktop/output/$company/result2/gf-outputs/gf-gau-waybackurl
+
+        clear
+
+
+        echo "---gf------loading---using--gau and waybackurl--output-file---"
+
+        sleep 4
+
+        echo "-----start----gf----xss"
+
+        gf xss ~/Desktop/output/$company/result2/all-url-gau-waybackurls.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-gau-waybackurl/xss-gf.txt
+        sleep 3
+        clear
+
+        echo "-----start----gf----redirect"
+
+        gf redirect ~/Desktop/output/$company/result2/all-url-gau-waybackurls.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-gau-waybackurl/redirect-gf.txt
+        sleep 3
+        clear
+
+        echo "-----start----gf----sqli"
+
+        gf sqli ~/Desktop/output/$company/result2/all-url-gau-waybackurls.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-gau-waybackurl/sqli-gf.txt
+        sleep 3
+        clear
+
+
+        echo "-----start----gf----idor"
+
+        gf idor ~/Desktop/output/$company/result2/all-url-gau-waybackurls.txt | tee ~/Desktop/output/$company/result2/gf-outputs/gf-gau-waybackurl/sqli-gf.txt
+        sleep 3
+        clear
+        echo "Finished"
+
+        
+
+    elif [ "$input" == "3" ]; then
+        echo "Exiting program"
+        exit 0
+    else
+        echo "Invalid input"
+    fi
+done
+
+
+
